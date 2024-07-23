79daa6e6a1b3d35f4f66cd04477627e1e21940a6
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-07-24 00:01:14 +0200

    24-Jul-24

--- Scenes/Menus/Options/Tabs/Audio.gd
+++ Scenes/Menus/Options/Tabs/Audio.gd
@@ -1,11 +1,11 @@
 extends Control
 
-@onready var master_volume_slider = $CenterContainer/VBoxContainer/Volume
+@onready var master_volume_slider = $CenterContainer/VBoxContainer/HBoxContainer/Volume
 
 
 func _ready():
 	var audio_config = ConfigHandler.load_audio_config()
-	master_volume_slider.value = audio_config["master_volume"]
+	master_volume_slider.value = audio_config ["master_volume"]
 
 
 func _on_volume_drag_ended(value_changed: bool):
